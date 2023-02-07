495425112fd0707d554cd0df86275107d0690266
lennonay <lennon.auyeung@me.com>
2023-02-06 17:28:00 -0800

    init

---
+++ src/model_explain.py
@@ -0,0 +1,9 @@
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+ 
+# some time later...
+ 
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
+print(result)
