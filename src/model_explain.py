67b21e14b22aa8f3a6e40ec1337ba43a427b2f5f
lennonay <lennon.auyeung@me.com>
2023-02-06 17:45:13 -0800

    added roc curve function

--- src/model_explain.py
+++ src/model_explain.py
@@ -7,3 +7,39 @@ pickle.dump(model, open(filename, 'wb'))
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, Y_test)
 print(result)
+
+
+#adopted from https://github.com/HarryShomer/xG-Model
+def get_roc(actual, predictions):
+    """
+    Get the roc curve (and auc score) for the different models
+    """
+    fig = plt.figure()
+    plt.title('ROC Curves')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+
+    colors = ['b', 'g', 'p']
+
+    for model, color in zip(predictions.keys(), colors):
+        # Convert preds to just prob of goal
+        preds = [pred[1] for pred in predictions[model]]
+
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(actual, preds)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+        plt.plot(false_positive_rate, true_positive_rate, label=' '.join([model + ':', str(round(roc_auc, 3))]))
+
+    # Add "Random" score
+    plt.plot([0, 1], [0, 1], 'r--', label=' '.join(["Random:", str(.5)]))
+
+    plt.legend(title='AUC Score', loc=4)
+    fig.savefig("ROC_xG.png")
+
+preds = {
+    "Random Forest": pipe_rf.predict_proba(X_test),
+    "Gradient Boosting": pip_gbc.predict_proba(X_test),
+    "Logistic Regression": pipe_lr_unopt.predict_proba(X_test)
+}
+get_roc(y_test, preds)
