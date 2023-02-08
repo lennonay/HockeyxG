37b67746a3d743abc3c55d9ce2d96ae168434d38
lennonay <lennon.auyeung@me.com>
2023-02-07 22:17:33 -0800

    modified modelling

--- src/modelling.py
+++ src/modelling.py
@@ -3,45 +3,47 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import (OneHotEncoder, StandardScaler)
 
-train_df = pd.read_csv('data/pre_processed/train_df.csv')
-test_df = pd.read_csv('data/pre_processed/test_df.csv')
+if __name__ == "__main__":
 
-X_train = train_df.drop('goal', axis = 1)
-y_train = train_df['goal']
+    train_df = pd.read_csv('data/pre_processed/train_df.csv')
+    test_df = pd.read_csv('data/pre_processed/test_df.csv')
 
-X_test = test_df.drop('goal', axis = 1)
-y_test = test_df['goal']
+    X_train = train_df.drop('goal', axis = 1)
+    y_train = train_df['goal']
 
-categorical_features  = ['shotType', 'lastEventCategory','team', 'shooterLeftRight']
-binary_features = ['isPlayoffGame','shootingTeamEmptyNet', 'defendingTeamEmptyNet']
+    X_test = test_df.drop('goal', axis = 1)
+    y_test = test_df['goal']
 
-numeric_features = ['arenaAdjustedShotDistance',
- 'distanceFromLastEvent',
- 'shotAngle',
- 'xCordAdjusted',
- 'yCordAdjusted','defendingTeamDefencemenOnIce','defendingTeamForwardsOnIce',
- 'defendingTeamGoals', 'shootingTeamDefencemenOnIce','shootingTeamForwardsOnIce','shootingTeamGoals']
+    categorical_features  = ['shotType', 'lastEventCategory','team', 'shooterLeftRight']
+    binary_features = ['isPlayoffGame','shootingTeamEmptyNet', 'defendingTeamEmptyNet']
 
-preprocessor = make_column_transformer(
-    (StandardScaler(),numeric_features),
-    (OneHotEncoder(drop="if_binary", dtype=int), binary_features), (make_pipeline(SimpleImputer(strategy="most_frequent"),OneHotEncoder(handle_unknown="ignore")), categorical_features)
-)
+    numeric_features = ['arenaAdjustedShotDistance',
+     'distanceFromLastEvent',
+     'shotAngle',
+     'xCordAdjusted',
+     'yCordAdjusted','defendingTeamDefencemenOnIce','defendingTeamForwardsOnIce',
+     'defendingTeamGoals', 'shootingTeamDefencemenOnIce','shootingTeamForwardsOnIce','shootingTeamGoals']
 
-#linear regression
-param_dist = {
-    "logisticregression__C": loguniform(1e-3, 1e3)
-}
+    preprocessor = make_column_transformer(
+        (StandardScaler(),numeric_features),
+        (OneHotEncoder(drop="if_binary", dtype=int), binary_features), (make_pipeline(SimpleImputer(strategy="most_frequent"),OneHotEncoder(handle_unknown="ignore")), categorical_features)
+    )
 
-pipe_lr_unopt = make_pipeline(
-    preprocessor, LogisticRegression(penalty='l2', random_state=234, max_iter=10000, tol=.01)
-)
+    #linear regression
+    param_dist = {
+        "logisticregression__C": loguniform(1e-3, 1e3)
+    }
 
-pipe_lr = RandomizedSearchCV(pipe_lr_unopt, param_dist, n_iter = 10, n_jobs = -1, return_train_score = True)
+    pipe_lr_unopt = make_pipeline(
+        preprocessor, LogisticRegression(penalty='l2', random_state=234, max_iter=10000, tol=.01)
+    )
 
-pip_lir.fit(X_train, y_train)
+    pipe_lr = RandomizedSearchCV(pipe_lr_unopt, param_dist, n_iter = 10, n_jobs = -1, return_train_score = True)
 
-lr_train_score = pipe_lr.score(X_train,y_train)
-lr_test_score = pipe_lr.score(X_test,y_test)
-lr_log_loss = log_loss(y_test,pipe_lr.predict_proba(X_test)[:,1])
-lr_roc = roc_auc_score(y_test, pipe_lr.predict_proba(X_test)[:, 1])
+    pip_lir.fit(X_train, y_train)
+
+    lr_train_score = pipe_lr.score(X_train,y_train)
+    lr_test_score = pipe_lr.score(X_test,y_test)
+    lr_log_loss = log_loss(y_test,pipe_lr.predict_proba(X_test)[:,1])
+    lr_roc = roc_auc_score(y_test, pipe_lr.predict_proba(X_test)[:, 1])
 
