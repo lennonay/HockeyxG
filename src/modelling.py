0b836d278f3e4934a063abb6500523b222a9a8ea
lennonay <lennon.auyeung@me.com>
2023-02-07 22:26:34 -0800

    modified packages

--- src/modelling.py
+++ src/modelling.py
@@ -2,8 +2,13 @@ import pandas as pd
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import (OneHotEncoder, StandardScaler)
-from sklearn.metrics import roc_auc_score, roc_curve, auc, log_loss
+from sklearn.metrics import roc_auc_score, log_loss
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.impute import SimpleImputer
+from scipy.stats import loguniform
+from sklearn.model_selection import GridSearchCV
 
 
 if __name__ == "__main__":
@@ -37,13 +42,13 @@ if __name__ == "__main__":
         "logisticregression__C": loguniform(1e-3, 1e3)
     }
 
-    pipe_lr_unopt = make_pipeline(
+    pipe_lr = make_pipeline(
         preprocessor, LogisticRegression(penalty='l2', random_state=234, max_iter=10000, tol=.01)
     )
 
-    pipe_lr = RandomizedSearchCV(pipe_lr_unopt, param_dist, n_iter = 10, n_jobs = -1, return_train_score = True)
+    pipe_lr = RandomizedSearchCV(pipe_lr, param_dist, n_iter = 10, n_jobs = -1, return_train_score = True)
 
-    pip_lir.fit(X_train, y_train)
+    pipe_lr.fit(X_train, y_train)
 
     lr_train_score = pipe_lr.score(X_train,y_train)
     lr_test_score = pipe_lr.score(X_test,y_test)
@@ -56,9 +61,9 @@ param_grid = {
     'gradientboostingclassifier__max_depth': [3, 4, 5]
 }
 
-pip_gbc_unopt = make_pipeline(preprocessor, GradientBoostingClassifier(random_state=123))
+pip_gbc = make_pipeline(preprocessor, GradientBoostingClassifier(random_state=123))
 
-pip_gbc = GridSearchCV(pip_gbc_unopt, param_grid=param_grid, cv=10)
+pip_gbc = GridSearchCV(pip_gbc, param_grid=param_grid, cv=10)
 
 pip_gbc.fit(X_train, y_train) 
 
@@ -68,13 +73,13 @@ gbc_log_loss = log_loss(y_test,pip_gbc.predict_proba(X_test)[:,1])
 gbc_roc = roc_auc_score(y_test, pip_gbc.predict_proba(X_test)[:, 1])
 
 #random forest
-pipe_rf_unopt = make_pipeline(preprocessor, RandomForestClassifier(n_estimators=100, random_state=123))
+pipe_rf = make_pipeline(preprocessor, RandomForestClassifier(n_estimators=100, random_state=123))
 
 param_grid = {
     'randomforestclassifier__min_samples_leaf': [50, 100, 250, 500]
 }
 
-pipe_rf = GridSearchCV(pipe_rf_unopt, param_grid=param_grid, cv=10)
+pipe_rf = GridSearchCV(pipe_rf, param_grid=param_grid, cv=10)
 
 pipe_rf.fit(X_train, y_train)
 
@@ -82,3 +87,4 @@ rf_train_score = pipe_rf.score(X_train,y_train)
 rf_test_score = pipe_rf.score(X_test,y_test)
 rf_log_loss = log_loss(y_test,pipe_rf.predict_proba(X_test)[:,1])
 rf_roc = roc_auc_score(y_test, pipe_rf.predict_proba(X_test)[:, 1])
+
